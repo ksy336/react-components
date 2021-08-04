90f48e0930f48b05f30dfe1f6a74f6b0e73da41c
Klimova Kseniya <ksuoxy773@gmail.com>
2021-08-04 16:08:55 +0400

    feat: add webpack to project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import "./index.css";
+import SearchBar from './components/search-bar';
+import Cards from "./components/cards";
+import React from "react";
+
+function App() {
+        return (
+            <div className="App">
+                <SearchBar />
+                <Cards />
+            </div>
+        );
+}
+
+export default App;
